80138423e3c224114be5dbfb1241bbebdf95fc44
leilalinoo <115218496+leilalinoo@users.noreply.github.com>
2023-02-23 15:07:46 +0100

    metodusok

---
+++ Metodus0223.java
@@ -0,0 +1,26 @@
+package metodus0223;
+
+public class Metodus0223 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void elso10szamosszege() {
+        
+        int osszeg = 0;
+        for(int i =0; i <10; i++){
+            osszeg +=i;
+        }
+        
+    }
+    
+    private static void osszead(int a, int b){
+        int osszead = a + b;
+        
+        
+    }
+    
+    
+
+}
