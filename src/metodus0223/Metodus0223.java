4805c0e52a26984e142b2eb2adebed03bfa09a73
leilalinoo <115218496+leilalinoo@users.noreply.github.com>
2023-02-23 15:18:07 +0100

    elso metodusok - uj commit

--- src/metodus0223/Metodus0223.java
+++ src/metodus0223/Metodus0223.java
@@ -1,20 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package metodus0223;
 
-/**
- *
- * @author aleil
- */
 public class Metodus0223 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
+    private static int elso10szamosszege() {
+
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += i;
+        }
+        return osszeg;
+    }
+
+    private static int osszead(int a, int b) {
+        return a + b;
+    }
+
+    private static void kiir(String szoveg) {
+        System.out.println(szoveg);
+    }
+
 }
