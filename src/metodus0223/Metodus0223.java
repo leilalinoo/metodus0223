e35a9fc08876a0d7a107ba3d07955ffe28d01c42
leilalinoo <115218496+leilalinoo@users.noreply.github.com>
2023-02-23 15:22:56 +0100

    kiiras

--- src/metodus0223/Metodus0223.java
+++ src/metodus0223/Metodus0223.java
@@ -3,7 +3,7 @@ package metodus0223;
 public class Metodus0223 {
 
     public static void main(String[] args) {
-
+        
     }
 
     private static int elso10szamosszege() {
@@ -12,10 +12,12 @@ public class Metodus0223 {
         for (int i = 0; i < 10; i++) {
             osszeg += i;
         }
+        kiir("Az első 10 szám összege: " + osszeg);
         return osszeg;
     }
 
     private static int osszead(int a, int b) {
+        kiir(a + " + " + b + " = " + (a + b));
         return a + b;
     }
 
